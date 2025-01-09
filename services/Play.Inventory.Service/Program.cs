b20f01ec33a8f98dd426b426a42d57f561f2ed0c
ritsth <akitirsth@gmail.com>
2025-01-09 16:48:48 -0500

    Commented out Grpc and using Http1 for RabbitMQ

--- services/Play.Inventory.Service/Program.cs
+++ services/Play.Inventory.Service/Program.cs
@@ -6,6 +6,9 @@ using Play.Catalog;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Repositories;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using MassTransit;
+using Play.Inventory.Service.Consumer;
+using MassTransit.RabbitMqTransport;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,22 +24,64 @@ BsonDefaults.GuidRepresentationMode = GuidRepresentationMode.V2;
 
 //Dependency injection (interface)
 builder.Services.AddScoped<IItemsRepository, ItemsRepository>();
+// builder.Services.AddScoped<ICatalogItemsRepository, CatalogItemsRepository>();
+// builder.Services.AddScoped<
 
 
-//Inter-services Communication using REST and HTTP
+//Inter-services Communication using REST and HTTP1
 builder.Services.AddHttpClient<CatalogClient>(client =>{
     client.BaseAddress= new Uri("http://localhost:5205");
 });
+//Handeling partial failure when communicating with other microservices 
+//1. Timeouts and Retry Policies (using Polly)
 // .AddPolicyHandler(Policy.TimeoutAsync<HttpRequestMessage>(1)); //one second timeout
+//2. Circuit Breaker Pattern
+// 3. Asynchronous Communication (implementted this using RabbitMQ)
 
 
-builder.Services.AddScoped<CatalogClientGrpc>();
+// //Grpc Communication stuff
+// builder.Services.AddScoped<CatalogClientGrpc>();
 
 // // Register gRPC client (Inter-services Communication)
-builder.Services.AddGrpcClient<Catalog.CatalogClient>(o =>
-{
-    o.Address = new Uri("https://localhost:5205"); // Address of the catalog service
+// builder.Services.AddGrpcClient<Catalog.CatalogClient>(o =>
+// {
+//     o.Address = new Uri("https://localhost:5205"); // Address of the catalog service
+// });
+
+
+//Configure RabbitMQ
+builder.Services.AddMassTransit(configure =>
+{        
+    // Register the consumer
+    configure.AddConsumer<CatalogItemCreatedConsumer>();
+    configure.AddConsumer<CatalogItemUpdatedConsumer>();
+
+    configure.UsingRabbitMq((context,configurator) =>
+    {
+        //not default so we need to configure it
+        // configurator.Host("rabbitmq", 5673, "/", h =>
+        // {
+        //     h.Username("guest");
+        //     h.Password("guest");
+        // });
+        configurator.Host("rabbitmq://localhost", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        // Configure consumer endpoints
+        configurator.ReceiveEndpoint("my-message-queue", e =>
+        {
+            e.ConfigureConsumer<CatalogItemCreatedConsumer>(context);
+            e.ConfigureConsumer<CatalogItemUpdatedConsumer>(context);
+
+        });
+    });
 });
+
+// Register MassTransit-related services
+builder.Services.AddMassTransitHostedService();
 
 
 var app = builder.Build();
